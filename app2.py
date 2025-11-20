123709fe0c43855cdb09815f68a69146217804dc
Tuta0515 <utaspring0515@icloud.com>
2025-11-20 16:22:30 +0900

    Create app2.py

---
+++ app2.py
@@ -0,0 +1,11 @@
+import stremlit as st
+
+st.title("こんにちは、吉村ゼミ”）
+
+name = st.text_imput("好きな言葉を入力してください")  
+st.write(name)
+
+camera = st.camera_input("写真を撮影します！")
+if camera:
+    st.image(camera, caption="写真", use_colimn_width=true)
+    
