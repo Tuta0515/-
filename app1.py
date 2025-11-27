a1902752df0413b6ab098f8ce63c32b15e6358de
Tuta0515 <utaspring0515@icloud.com>
2025-11-27 15:41:00 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -7,10 +7,20 @@ st.write(name)
 
 st.checkbox("同意します")
 
+address = st.selectbox("次の中から現住所を教えてください",["映画","音楽","散歩"])
+st.write(hobby)
+
+st.slider("この映画を10点満点で評価してください",0,10,5)
+st.write(score)
+
+st.ragio("性別を選択してください")
+
+
 camera = st.camera_input("写真を撮影します")
 if camera:
     st.image(camera,caption="写真", use_colimn_width=True)
-    
+
+
 
 
 
