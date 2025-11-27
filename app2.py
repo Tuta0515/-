99923b2dc5e57265975f642b3869b040120c8acd
Tuta0515 <utaspring0515@icloud.com>
2025-11-27 15:17:27 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -5,7 +5,7 @@ st.title("こんにちは、吉村ゼミ”）
 name = st.text_imput("好きな言葉を入力してください")  
 st.write(name)
 
-camera = st.camera_input("写真を撮影します！")
+camera = st.camera_input("写真を撮影します")
 if camera:
-  st.image(camera,caption="写真", use_colimn_width=True)
+    st.image(camera,caption="写真", use_colimn_width=True)
     
