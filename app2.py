9e24f549aac0f0687457e09f22e0387cce516dea
Tuta0515 <utaspring0515@icloud.com>
2025-11-27 15:14:12 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -7,5 +7,5 @@ st.write(name)
 
 camera = st.camera_input("写真を撮影します！")
 if camera:
-   st.image(camera,caption="写真", use_colimn_width=True)
+  st.image(camera,caption="写真", use_colimn_width=True)
     
